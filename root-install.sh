5665625c8446bd82c8369e511e9e183f496f61a1
imbolc <imbolc@imbolc.name>
2018-06-16 11:07:29 +0700

    Up

--- root-install.sh
+++ root-install.sh
@@ -1,35 +1,34 @@
 #!/bin/bash
 
-apt-get install -y aptitude
-aptitude update
-aptitude upgrade -y
+apt update
+apt upgrade -y
 
 
 echo "=== INSTALL PACKAGES"
 # core
-aptitude install -y sudo tmux curl htop cron mc ranger mosh wrk rsync
+apt install -y sudo tmux curl htop cron mc ranger mosh wrk rsync
 
 # web
-aptitude install -y nginx supervisor
+apt install -y nginx supervisor
 
 # build
-aptitude install -y gcc build-essential
+apt install -y gcc build-essential
 
 # python extensions
-aptitude install -y libssl-dev libbz2-dev libreadline-dev libsqlite3-dev libffi-dev zlib1g-dev tk-dev
+apt install -y libssl-dev libbz2-dev libreadline-dev libsqlite3-dev libffi-dev zlib1g-dev tk-dev
 
 # build PIL from source
-aptitude install -y libjpeg-dev libfreetype6-dev
+apt install -y libjpeg-dev libfreetype6-dev
 
 # pyaml speedup
 apt install -y libyaml-dev
 
 # base python
-aptitude install -y python python-setuptools python-dev
+apt install -y python python-setuptools python-dev
 
 
 echo "=== LOCALES"
-aptitude install -y locales 
+apt install -y locales 
 echo "LANG=en_US.UTF-8" > /etc/default/locale
 cat > /etc/locale.gen << EOF
 en_US.UTF-8 UTF-8
@@ -39,7 +38,7 @@ locale-gen
 
 
 echo "=== GIT"
-aptitude install -y git-core
+apt install -y git-core
 git config --global core.editor "vim"
 git config --global alias.ci commit
 git config --global alias.st status
@@ -47,8 +46,8 @@ git config --global alias.co checkout
 
 
 echo "=== VIM"
-aptitude purge -y vim
-aptitude install -y neovim python-pip python-dev python3-dev python3-venv
+apt purge -y vim
+apt install -y neovim python-pip python-dev python3-dev python3-venv
 
 rm -R ~/.vim ~/.vimrc
 git clone https://github.com/imbolc/.vim ~/.vim
@@ -65,5 +64,5 @@ update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
 update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
 
 echo "=== NODE"
-curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
-sudo aptitude install -y nodejs
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo apt install -y nodejs
